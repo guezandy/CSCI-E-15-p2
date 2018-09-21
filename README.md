df2e3af0dbc141a08c29e2d218d3e43d418ea877
Andrew Rodriguez <andrewr@twitter.com>
2018-09-20 19:29:46 -0700

    Organize files and add readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pace Math
++ By: Andrew Rodriguez
++ Production URL: <http://p2.squareinches.com>
+
+## Outside resources
++ Styling via [Bootstrap CDN](https://www.bootstrapcdn.com)
+
+## 3 Unique inputs
+1. Text input for distance running
+2. Text input for goal time
+2. Dropdown selecting kilomters or miles
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require 'logic.php';
+require 'calculate.php';
+?>
+
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <title>Pace math</title>
+    <meta charset='utf-8'>
+
+    <link href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'
+          rel='stylesheet'
+          integrity='sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO'
+          crossorigin='anonymous'>
+
+    <link href='/styles/styles.css' rel='stylesheet'>
+</head>
+<body>
+
+</body>
+</html>
